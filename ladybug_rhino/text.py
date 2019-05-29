5b28e81d409dc1589c4d07d0a3a14868f64d19c9
Chris <chris@ladybug.tools>
2019-05-29 16:09:30 -0400

    feat(text): Adding functions to display text in the Rhino scene

---
+++ ladybug_rhino/text.py
@@ -0,0 +1,188 @@
+"""Functions to add text to the Rhino scene and create Grasshopper text objects."""
+
+from .fromgeometry import from_plane
+
+try:
+    import Rhino as rh
+    import scriptcontext as sc
+    import System.Guid as guid
+except ImportError as e:
+    raise ImportError(
+        "Failed to import Rhino.\n{}".format(e))
+try:
+    import Grasshopper as gh
+except ImportError:
+    print('Failed to import Grasshopper.\n'
+          'Only functions for adding text to Rhino will be availabe.')
+
+from math import sqrt
+
+
+def rhino_text(text, plane, height, font='Arial',
+               horizontal_alignment=0, vertical_alignment=5,
+               attributes=None):
+    """Add text to the Rhino sceneusing a text string and ladybug Plane.
+
+    Args:
+        text: A text string to be added to the Rhino scene.
+        plane: A Ladybug Plane object to locate and orient the text in the Rhino scene.
+        height: A number for the height of the text in the Rhino scene.
+        font: An optional text string for the font in which to draw the text.
+        horizontal_alignment: An optional integer to specify the horizontal alignment
+             of the text. Choose from: (0 = Left, 1 = Center, 2 = Right)
+        vertical_alignment: An optional integer to specify the vertical alignment
+             of the text. Choose from: (0 = Top, 1 = MiddleOfTop, 2 = BottomOfTop,
+             3 = Middle, 4 = MiddleOfBottom, 5 = Bottom, 6 = BottomOfBoundingBox)
+        attributes: Optional attributes for adding to the Rhino scene.
+    """
+    txt = rh.Display.Text3d(text, from_plane(plane), height)
+    txt.FontFace = font
+    txt.HorizontalAlignment = AlignmentTypes.horizontal(horizontal_alignment)
+    txt.VerticalAlignment = AlignmentTypes.vertical(vertical_alignment)
+    if attributes is None:
+        attributes = sc.doc.CreateDefaultAttributes()
+    id = sc.doc.Objects.AddText(txt, attributes)
+    if id != guid.Empty:
+        sc.doc.Views.Redraw()
+        return True
+    return False
+
+
+def grasshopper_text(text, plane, height, font='Arial',
+                     horizontal_alignment=0, vertical_alignment=5):
+    """Generate a Bake-able Grasshopper text object from a text string and ladybug Plane.
+
+    Args:
+        text: A text string to be converted to a a Grasshopper text object.
+        plane: A Ladybug Plane object to locate and orient the text in the Rhino scene.
+        height: A number for the height of the text in the Rhino scene.
+        font: An optional text string for the font in which to draw the text.
+        horizontal_alignment: An optional integer to specify the horizontal alignment
+             of the text. Choose from: (0 = Left, 1 = Center, 2 = Right)
+        vertical_alignment: An optional integer to specify the vertical alignment
+             of the text. Choose from: (0 = Top, 1 = MiddleOfTop, 2 = BottomOfTop,
+             3 = Middle, 4 = MiddleOfBottom, 5 = Bottom, 6 = BottomOfBoundingBox)
+    """
+    txt = rh.Display.Text3d(text, from_plane(plane), height)
+    txt.FontFace = font
+    txt.HorizontalAlignment = AlignmentTypes.horizontal(horizontal_alignment)
+    txt.VerticalAlignment = AlignmentTypes.vertical(vertical_alignment)
+    return TextGoo(txt)
+
+
+class TextGoo(gh.Kernel.Types.GH_GeometricGoo[rh.Display.Text3d],
+              gh.Kernel.IGH_BakeAwareData, gh.Kernel.IGH_PreviewData):
+    """A Text object that can be baked and transformed in Grasshopper.
+
+    The code for this entire class was taken from David Rutten and Giulio Piacentino's
+    script described here:
+    https://discourse.mcneel.com/t/creating-text-objects-and-outputting-them-as-normal-rhino-geometry/47834/7
+    """
+
+    def __init__(self, text):
+        """Initialize Bake-able text.
+
+        Args:
+            text: A Rhino Text3d object.
+        """
+        self.m_value = text
+
+    @staticmethod
+    def DuplicateText3d(original):
+        if original is None:
+            return None
+        text = rh.Display.Text3d(original.Text, original.TextPlane, original.Height)
+        text.Bold = original.Bold,
+        text.Italic = original.Italic,
+        text.FontFace = original.FontFace
+        return text
+
+    def DuplicateGeometry(self):
+        return TextGoo(TextGoo.DuplicateText3d(self.m_value))
+
+    def get_TypeName(self):
+        return "3D Text"
+
+    def get_TypeDescription(self):
+        return "3D Text"
+
+    def ToString(self):
+        if self.m_value is None:
+            return "<null>"
+        return self.m_value.Text
+
+    def get_Boundingbox(self):
+        if self.m_value is None:
+            return rh.Geometry.BoundingBox.Empty
+        return self.m_value.BoundingBox
+
+    def GetBoundingBox(self, xform):
+        if self.m_value is None:
+            return rh.Geometry.BoundingBox.Empty
+        box = self.m_value.BoundingBox
+        corners = xform.TransformList(box.GetCorners())
+        return rh.Geometry.BoundingBox(corners)
+
+    def Transform(self, xform):
+        text = TextGoo.DuplicateText3d(self.m_value)
+        if text is None:
+            return TextGoo(None)
+
+        plane = text.TextPlane
+        point = plane.PointAt(1, 1)
+
+        plane.Transform(xform)
+        point.Transform(xform)
+        dd = point.DistanceTo(plane.Origin)
+
+        text.TextPlane = plane
+        text.Height *= dd / sqrt(2)
+        return TextGoo(text)
+
+    def Morph(self, xmorph):
+        return self.DuplicateGeometry()
+
+    def get_ClippingBox(self):
+        return self.get_Boundingbox()
+
+    def DrawViewportWires(self, args):
+        if self.m_value is None:
+            return
+        args.Pipeline.Draw3dText(self.m_value, args.Color)
+
+    def DrawViewportMeshes(self, args):
+        # Do not draw in meshing layer.
+        pass
+
+    def BakeGeometry(self, doc, att, id):
+        id = guid.Empty
+        if self.m_value is None:
+            return False, id
+        if att is None:
+            att = doc.CreateDefaultAttributes()
+        id = doc.Objects.AddText(self.m_value, att)
+        return True, id
+
+
+class AlignmentTypes(object):
+    """Enumeration of text alignment types."""
+
+    HORIZONTAL = {0: rh.DocObjects.TextHorizontalAlignment.Left,
+                  1: rh.DocObjects.TextHorizontalAlignment.Center,
+                  2: rh.DocObjects.TextHorizontalAlignment.Right}
+
+    VERTICAL = {0: rh.DocObjects.TextVerticalAlignment.Top,
+                1: rh.DocObjects.TextVerticalAlignment.MiddleOfTop,
+                2: rh.DocObjects.TextVerticalAlignment.BottomOfTop,
+                3: rh.DocObjects.TextVerticalAlignment.Middle,
+                4: rh.DocObjects.TextVerticalAlignment.MiddleOfBottom,
+                5: rh.DocObjects.TextVerticalAlignment.Bottom,
+                6: rh.DocObjects.TextVerticalAlignment.BottomOfBoundingBox}
+
+    @classmethod
+    def horizontal(cls, field_number):
+        return cls.HORIZONTAL[field_number]
+
+    @classmethod
+    def vertical(cls, field_number):
+        return cls.VERTICAL[field_number]
--- ladybug_rhino/togeometry.py
+++ ladybug_rhino/togeometry.py
@@ -1,150 +1,218 @@
-"""Functions to create Rhino geometries from Ladybug geometries."""
+"""Functions to create Ladybug geometries from Rhino geometries."""
 
 try:
+    from ladybug_geometry.geometry2d.pointvector import Vector2D, Point2D
+    from ladybug_geometry.geometry2d.ray import Ray2D
+    from ladybug_geometry.geometry2d.line import LineSegment2D
+    from ladybug_geometry.geometry2d.polygon import Polygon2D
+    from ladybug_geometry.geometry2d.mesh import Mesh2D
+    from ladybug_geometry.geometry3d.pointvector import Vector3D, Point3D
+    from ladybug_geometry.geometry3d.ray import Ray3D
+    from ladybug_geometry.geometry3d.line import LineSegment3D
+    from ladybug_geometry.geometry3d.plane import Plane
+    from ladybug_geometry.geometry3d.mesh import Mesh3D
+    from ladybug_geometry.geometry3d.face import Face3D
+    from ladybug_geometry.geometry3d.polyface import Polyface3D
+except ImportError as e:
+    raise ImportError(
+        "Failed to import ladybug_geometry.\n{}".format(e))
+try:
+    import ladybug.color as lbc
+except ImportError as e:
+    raise ImportError(
+        "Failed to import ladybug.\n{}".format(e))
+try:
     import Rhino.Geometry as rg
-except ImportError as e:
-    raise ImportError(
-        "Failed to import Rhino.\n{}".format(e))
-try:
-    from ladybug_dotnet.dotnet import color_to_color, gray
-except ImportError as e:
-    raise ImportError(
-        "Failed to import ladybug_dotnet.\n{}".format(e))
-try:
     import scriptcontext
     tolerance = scriptcontext.doc.ModelAbsoluteTolerance
 except ImportError as e:
     raise ImportError(
-        "Failed to import scriptcontext.\n{}".format(e))
+        "Failed to import Rhino.\n{}".format(e))
 
 
 """____________2D GEOMETRY TRANSLATORS____________"""
 
 
-def from_vector2d(vector):
-    """Rhino Vector3d from ladybug Vector2D."""
-    return rg.Vector3d(vector.x, vector.y, 0)
+def to_vector2d(vector):
+    """Ladybug Vector2D from Rhino Vector3d."""
+    return Vector2D(vector.X, vector.Y)
 
 
-def from_point2d(point, z=0):
-    """Rhino Point3d from ladybug Point2D."""
-    return rg.Point3d(point.x, point.y, z)
+def to_point2d(point):
+    """Ladybug Point2D from Rhino Point3d."""
+    return Point2D(point.X, point.Y)
 
 
-def from_ray2d(ray, z=0):
-    """Rhino Ray3d from ladybug Ray2D."""
-    return rg.Ray3d(from_point2d(ray.p, z), from_vector2d(ray.v))
+def to_ray2d(ray):
+    """Ladybug Ray2D from Rhino Ray3d."""
+    return Ray2D(to_point2d(ray.Position), to_vector2d(ray.Direction))
 
 
-def from_linesegment2d(line, z=0):
-    """Rhino LineCurve from ladybug LineSegment2D."""
-    return rg.LineCurve(from_point2d(line.p1, z), from_point2d(line.p2, z))
+def to_linesegment2d(line):
+    """Ladybug LineSegment2D from Rhino LineCurve."""
+    return LineSegment2D.from_end_points(
+        to_point2d(line.PointAtStart), to_point2d(line.PointAtEnd))
 
 
-def from_polygon2d(polygon, z=0):
-    """Rhino closed PolyLineCurve from ladybug Polygon2D."""
-    return rg.PolylineCurve([from_point2d(pt, z) for pt in polygon.vertices] +
-                            [from_point2d(polygon[0], z)])
+def to_polygon2d(polygon):
+    """Ladybug Polygon2D from Rhino closed PolyLineCurve."""
+    assert polygon.IsClosed, \
+        'Rhino PolyLineCurve must be closed to make a Ladybug Polygon2D.'
+    return Polygon2D(
+        [to_point2d(polygon.Point(i)) for i in range(polygon.PointCount - 1)])
 
 
-def from_mesh2d(mesh, z=0):
-    """Rhino Mesh from ladybug Mesh2D."""
-    pt_function = _get_point2d_function(z)
-    return _translate_mesh(mesh, pt_function)
-
-
-def _get_point2d_function(z_val):
-    def point2d_function(pt):
-        return from_point2d(pt, z_val)
-    return point2d_function
+def to_mesh2d(mesh, color_by_face=True):
+    """Ladybug Mesh2D from Rhino Mesh."""
+    lb_verts = tuple(to_point2d(pt) for pt in mesh.Vertices)
+    lb_faces, colors = _extract_mesh_faces_colors(mesh, color_by_face)
+    return Mesh2D(lb_verts, lb_faces, colors)
 
 
 """____________3D GEOMETRY TRANSLATORS____________"""
 
 
-def from_vector3d(vector):
-    """Rhino Vector3d from ladybug Vector3D."""
-    return rg.Vector3d(vector.x, vector.y, vector.z)
+def to_vector3d(vector):
+    """Ladybug Vector3D from Rhino Vector3d."""
+    return Vector3D(vector.X, vector.Y, vector.Z)
 
 
-def from_point3d(point):
-    """Rhino Point3d from ladybug Point3D."""
-    return rg.Point3d(point.x, point.y, point.z)
+def to_point3d(point):
+    """Ladybug Point3D from Rhino Point3d."""
+    return Point3D(point.X, point.Y, point.Z)
 
 
-def from_ray3d(ray, z=0):
-    """Rhino Ray3d from ladybug Ray2D."""
-    return rg.Ray3d(from_point3d(ray.p, z), from_vector3d(ray.v))
+def to_ray3d(ray):
+    """Ladybug Ray3D from Rhino Ray3d."""
+    return Ray3D(to_point3d(ray.Position), to_vector3d(ray.Direction))
 
 
-def from_linesegment3d(line):
-    """Rhino LineCurve from ladybug LineSegment3D."""
-    return rg.LineCurve(from_point3d(line.p1), from_point3d(line.p2))
+def to_linesegment3d(line):
+    """Ladybug LineSegment3D from Rhino LineCurve."""
+    return LineSegment3D(
+        to_point3d(line.PointAtStart), to_point3d(line.PointAtEnd))
 
 
-def from_plane(pl):
-    """Rhino Plane from ladybug Plane."""
-    return rg.Plane(from_point3d(pl.o), from_vector3d(pl.x), from_vector3d(pl.y))
+def to_plane(pl):
+    """Ladybug Plane from Rhino Plane."""
+    return Plane(
+        to_vector3d(pl.ZAxis), to_point3d(pl.Origin), to_vector3d(pl.XAxis))
 
 
-def from_mesh3d(mesh):
-    """Rhino Mesh from ladybug Mesh3D."""
-    return _translate_mesh(mesh, from_point3d)
+def to_mesh3d(mesh, color_by_face=True):
+    """Ladybug Mesh3D from Rhino Mesh."""
+    lb_verts = tuple(to_point3d(pt) for pt in mesh.Vertices)
+    lb_faces, colors = _extract_mesh_faces_colors(mesh, color_by_face)
+    return Mesh3D(lb_verts, lb_faces, colors)
 
 
-def from_face3d(face):
-    """Rhino Brep from ladybug Face3D."""
-    segs = [from_linesegment3d(seg) for seg in face.boundary_segments]
-    brep = rg.Brep.	CreatePlanarBreps(segs, tolerance)[0]
-    if face.has_holes:
-        for hole in face.hole_segments:
-            trim_crvs = [from_linesegment3d(seg) for seg in hole]
-            brep.Loops.AddPlanarFaceLoop(0, rg.BrepLoopType.Inner, trim_crvs)
-    return brep
+def to_face3d(brep, meshing_parameters=None):
+    """List of Ladybug Face3D objects from a Rhino Brep.
 
-
-def from_polyface3d(polyface):
-    """Rhino Brep from ladybug Polyface3D."""
-    rh_faces = [from_face3d(face) for face in polyface.faces]
-    brep = rg.Brep.JoinBreps(rh_faces, tolerance)
-    if len(brep) == 1:
-        return brep[0]
-
-
-def _translate_mesh(mesh, pt_function):
-    """Translates both 2D and 3D meshes to Rhino"""
-    rhino_mesh = rg.Mesh()
-    if mesh.is_color_by_face:  # Mesh is constructed face-by-face
-        _f_num = 0
-        for face in mesh.faces:
-            for pt in tuple(mesh[i] for i in face):
-                rhino_mesh.Vertices.Add(pt_function(pt))
-            if len(face) == 4:
-                rhino_mesh.Faces.AddFace(_f_num, _f_num + 1, _f_num + 2, _f_num + 3)
-                _f_num += 4
-            else:
-                rhino_mesh.Faces.AddFace(_f_num, _f_num + 1, _f_num + 2)
-                _f_num += 3
-        if mesh.colors is not None:
-            rhino_mesh.VertexColors.CreateMonotoneMesh(gray())
-            _f_num = 0
-            for i, face in enumerate(mesh.faces):
-                col = color_to_color(mesh.colors[i])
-                rhino_mesh.VertexColors[_f_num] = col
-                rhino_mesh.VertexColors[_f_num + 1] = col
-                rhino_mesh.VertexColors[_f_num + 2] = col
-                if len(face) == 4:
-                    rhino_mesh.VertexColors[_f_num + 3] = col
-                    _f_num += 4
+    Args:
+        brep: A Rhino Brep that will be converted into a list of Ladybug Face3D.
+        meshing_parameters: Optional Rhino Meshing Parameters to describe how
+            curved faces should be convereted into planar elements. If None,
+            Rhino's Default Meshing Parameters will be used.
+    """
+    meshing_parameters = meshing_parameters or rg.MeshingParameters.Default
+    faces = []
+    for b_face in brep.Faces:
+        if b_face.IsPlanar(tolerance):
+            all_verts = []
+            for count in range(b_face.Loops.Count):  # Each loop is a face boundary/hole
+                success, loop_pline = \
+                    b_face.Loops.Item[count].To3dCurve().TryGetPolyline()
+                if not success:  # If we failed to get a polyline, there are curved edges
+                    loop_pcrv = b_face.Loops.Item[count].To3dCurve()
+                    f_norm = b_face.NormalAt(0, 0)
+                    if f_norm.Z < 0:
+                        loop_pcrv.Reverse()
+                    loop_verts = []
+                    try:
+                        loop_pcrvs = [loop_pcrv.SegmentCurve(i)
+                                      for i in range(loop_pcrv.SegmentCount)]
+                    except Exception:
+                        try:
+                            loop_pcrvs = [loop_pcrv[0]]
+                        except Exception:
+                            loop_pcrvs = [loop_pcrv]
+                    for seg in loop_pcrvs:
+                        if seg.Degree == 1:
+                            loop_verts.append(to_point3d(seg.PointAtStart))
+                        else:
+                            # Ensure curve subdivisions align with adjacent curved faces
+                            seg_mesh = rg.Mesh.CreateFromSurface(
+                                rg.Surface.CreateExtrusion(seg, f_norm),
+                                meshing_parameters)
+                            for i in range(seg_mesh.Vertices.Count / 2 - 1):
+                                loop_verts.append(to_point3d(seg_mesh.Vertices[i]))
+                    all_verts.append(loop_verts)
                 else:
-                    _f_num += 3
-    else:  # Mesh is constructed vertex-by-vertex
-        for pt in mesh.vertices:
-            rhino_mesh.Vertices.Add(pt_function(pt))
-        for face in mesh.faces:
-            rhino_mesh.Faces.AddFace(*face)
-        if mesh.colors is not None:
-            rhino_mesh.VertexColors.CreateMonotoneMesh(gray())
-            for i, col in enumerate(mesh.colors):
-                rhino_mesh.VertexColors[i] = color_to_color(col)
-    return rhino_mesh
+                    all_verts.append([to_point3d(loop_pline.Item[i])
+                                      for i in range(loop_pline.Count - 1)])
+            if len(all_verts) == 1:  # No holes in the shape
+                faces.append(Face3D.from_vertices(all_verts[0]))
+            else:  # There's at least one hole in the shape
+                faces.append(
+                    Face3D.from_shape_with_holes(all_verts[0], all_verts[1:]))
+        else:
+            if b_face.OrientationIsReversed:
+                b_face.Reverse(0, True)
+            face_brep = b_face.DuplicateFace(True)
+            meshed_brep = rg.Mesh.CreateFromBrep(face_brep, meshing_parameters)[0]
+            for m_face in meshed_brep.Faces:
+                if m_face.IsQuad:
+                    lb_face = Face3D.from_vertices(
+                        tuple(to_point3d(meshed_brep.Vertices[i]) for i in
+                              (m_face.A, m_face.B, m_face.C, m_face.D)))
+                    if lb_face.validate_planarity(tolerance, False):
+                        faces.append(lb_face)
+                    else:
+                        lb_face_1 = Face3D.from_vertices(
+                            tuple(to_point3d(meshed_brep.Vertices[i]) for i in
+                                  (m_face.A, m_face.B, m_face.C)))
+                        lb_face_2 = Face3D.from_vertices(
+                            tuple(to_point3d(meshed_brep.Vertices[i]) for i in
+                                  (m_face.C, m_face.D, m_face.A)))
+                        faces.extend([lb_face_1, lb_face_2])
+                else:
+                    lb_face = Face3D.from_vertices(
+                        tuple(to_point3d(meshed_brep.Vertices[i]) for i in
+                              (m_face.A, m_face.B, m_face.C)))
+                    faces.append(lb_face)
+    return faces
+
+
+def to_polyface3d(brep, meshing_parameters=None):
+    """A Ladybug Polyface3D object from a Rhino Brep.
+
+    Args:
+        brep: A Rhino Brep that will be converted into a list of Ladybug Face3D.
+        meshing_parameters: Optional Rhino Meshing Parameters to describe how
+            curved faces should be convereted into planar elements. If None,
+            Rhino's Default Meshing Parameters will be used.
+    """
+    lb_faces = to_face3d(brep)
+    return Polyface3D.from_faces_tolerance(lb_faces, tolerance)
+
+
+def _extract_mesh_faces_colors(mesh, color_by_face):
+    colors = None
+    lb_faces = []
+    for face in mesh.Faces:
+        if face.IsQuad:
+            lb_faces.append((face[0], face[1], face[2], face[3]))
+        else:
+            lb_faces.append((face[0], face[1], face[2]))
+
+    if len(mesh.VertexColors) != 0:
+        colors = []
+        if color_by_face is True:
+            for face in mesh.Faces:
+                col = mesh.VertexColors[face[0]]
+                colors.append(lbc.Color(col.R, col.G, col.B))
+        else:
+            for col in mesh.VertexColors:
+                colors.append(lbc.Color(col.R, col.G, col.B))
+    return lb_faces, colors
