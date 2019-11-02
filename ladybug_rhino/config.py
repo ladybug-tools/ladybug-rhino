27b6004484e119aef4dceed46d31d926236226f0
Chris <chris@ladybug.tools>
2019-11-02 00:36:01 -0400

    feat(config): Add Rhino model units system to the config file

--- ladybug_rhino/config.py
+++ ladybug_rhino/config.py
@@ -1,13 +1,51 @@
 """Ladybug_rhino configurations.
-Global variables such as tolerances are stored here.
+Global variables such as tolerances and units are stored here.
 """
 
-try:  # try to import tolerance from the active Rhino document
+try:  # Try to import tolerance from the active Rhino document
     import scriptcontext
     tolerance = scriptcontext.doc.ModelAbsoluteTolerance
     angle_tolerance = scriptcontext.doc.ModelAngleToleranceRadians
-except ImportError:  # no Rhino doc is available. Default to Rhino' default.
+except ImportError:  # No Rhino doc is available. Use Rhino's default.
     tolerance = 0.01
     angle_tolerance = 0.01745  # default is 1 degree
     print('Failed to import Rhino scriptcontext. Default tolerance of {} '
           'and angle tolerance of {} will be used.'.format(tolerance, angle_tolerance))
+
+
+def conversion_factor():
+    """Get the conversion factor to meters based on the current Rhino doc units system.
+
+    Returns:
+        A number for the conversion factor, which should be multiplied by all distance
+        units taken from Rhino geoemtry in order to convert them to meters.
+    """
+    try:  # Try to import tolerance from the active Rhino document
+        import scriptcontext
+        units = str(scriptcontext.doc.ModelUnitSystem).split('.')[-1]
+    except ImportError:  # No Rhino doc available. Default to the greatest of all units
+        units = 'Meters'
+
+    if units == 'Meters':
+        return 1.0
+    elif units == 'Millimeters':
+        return 0.001
+    elif units == 'Feet':
+        return 0.305
+    elif units == 'Inches':
+        return 0.0254
+    elif units == 'Centimeters':
+        return 0.01
+    else:
+        raise ValueError(
+            "You're kidding me! What units are you using?" + units + "?\n"
+            "Please use Meters, Millimeters, Centimeters, Feet or Inches.")
+
+
+def units_system():
+    """Get text for the current Rhino doc units system. (eg. 'Meters', 'Feet')"""
+    try:  # Try to import tolerance from the active Rhino document
+        import scriptcontext
+        return str(scriptcontext.doc.ModelUnitSystem).split('.')[-1]
+    except ImportError:  # No Rhino doc available. Default to the greatest of all units
+        return 'Meters'
