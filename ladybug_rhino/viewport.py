af4c2b055b094240e01d9ce1f51950dce5befc41
Chris <chris@ladybug.tools>
2020-05-07 01:39:59 -0400

    feat(viewport): Add more methods to viewport module

--- ladybug_rhino/viewport.py
+++ ladybug_rhino/viewport.py
@@ -2,7 +2,6 @@
 
 try:
     import Rhino.Geometry as rg
-    import Rhino.Display as rd
 except ImportError as e:
     raise ImportError("Failed to import Rhino.\n{}".format(e))
 
@@ -11,6 +10,8 @@ try:  # Try to import tolerance from the active Rhino document
 except ImportError:  # No Rhino doc is available. Use Rhino's default.
     raise ImportError("Failed to import Rhino scriptcontext.\n{}".format(e))
 
+import math
+
 
 def camera_oriented_plane(origin):
     """Get a Rhino Plane that is oriented facing the camera.
@@ -18,6 +19,98 @@ def camera_oriented_plane(origin):
     Args:
         origin: A Rhino Point for the origin of the plane.
     """
-    camera_x = sc.doc.Views.ActiveView.ActiveViewport.CameraX
-    camera_y = sc.doc.Views.ActiveView.ActiveViewport.CameraY
+    active_view = sc.doc.Views.ActiveView.ActiveViewport
+    camera_x = active_view.CameraX
+    camera_y = active_view.CameraY
     return rg.Plane(origin, camera_x, camera_y)
+
+
+def viewport_by_name(view_name=None):
+    """Get a Rhino Viewport object using the name of the viewport.
+
+    Args:
+        view_name: Text for the name of the Rhino Viewport. If None, the
+            current Rhino viewport will be used.
+    """
+    try:
+        return sc.doc.Views.Find(view_name, False).ActiveViewport if view_name is not None \
+            else sc.doc.Views.ActiveView.ActiveViewport
+    except:
+        raise ValueError('Viewport "{}" was not found in the Rhino '
+                         'document.'.format(view_name))
+
+
+def viewport_vh_vv(viewport, view_type):
+    """Get the horizontal angle (vh) and the vertical angle (vv) from a viewport.
+    
+    Args:
+        viewport: A Rhino ViewPort object for which properties will be extracted.
+        view_type: An integer to set the view type (-vt). Choose from the
+            choices below.
+                * 0 Perspective (v)
+                * 1 Hemispherical fisheye (h)
+                * 2 Parallel (l)
+                * 3 Cylindrical panorama (c)
+                * 4 Angular fisheye (a)
+                * 5 Planisphere [stereographic] projection (s)
+    """
+    if view_type == 0:  # perspective
+        right_vec = viewport.GetFrustumRightPlane()[1][1]
+        left_vec = viewport.GetFrustumLeftPlane()[1][1]
+        h_angle = 180 - math.degrees(rg.Vector3d.VectorAngle(right_vec, left_vec))
+        bottom_vec = viewport.GetFrustumBottomPlane()[1][1]
+        top_vec = viewport.GetFrustumTopPlane()[1][1]
+        v_angle = 180 - math.degrees(rg.Vector3d.VectorAngle(bottom_vec, top_vec))
+        return h_angle, v_angle
+    if view_type == 1 or view_type == 5:
+        return 180, 180
+    if view_type == 2:
+        v_rect = viewport.GetNearRect()
+        return int(v_rect[0].DistanceTo(v_rect[1])), int(v_rect[0].DistanceTo(v_rect[2]))
+    if view_type == 3:
+        return 360, 180
+    if view_type == 4:
+        return 60, 60
+
+
+def viewport_properties(viewport, view_type=None):
+    """Get a dictionary of properties of a Rhino viewport.
+
+    Args:
+        viewport: A Rhino ViewPort object for which properties will be extracted.
+        view_type: An integer to set the view type (-vt). Choose from the
+            choices below or set to None to have it derived from the viewport.
+                * 0 Perspective (v)
+                * 1 Hemispherical fisheye (h)
+                * 2 Parallel (l)
+                * 3 Cylindrical panorama (c)
+                * 4 Angular fisheye (a)
+                * 5 Planisphere [stereographic] projection (s)
+
+    Returns:
+        A dictionary with the following keys: 'view_type', 'position', 'direction',
+        'up_vector', 'h_angle', 'v_angle'
+        
+    """
+    # ensure that we have an integer for the view_type
+    if view_type is None:
+        view_type = 2 if viewport.IsParallelProjection else 0
+
+    # get the position, direction and up vectors
+    pos = viewport.CameraLocation
+    direct = viewport.CameraDirection
+    up_vec = viewport.CameraUp
+    direct.Unitize()
+    up_vec.Unitize()
+
+    # get the h_angle and v_angle from the viewport
+    h_angle, v_angle = viewport_vh_vv(viewport, view_type)
+
+    return {
+        'view_type': view_type,
+        'position': (pos.X, pos.Y, pos.Z),
+        'direction': (direct.X, direct.Y, direct.Z),
+        'up_vector': (up_vec.X, up_vec.Y, up_vec.Z),
+        'h_angle': h_angle,
+        'v_angle': v_angle
+        }
